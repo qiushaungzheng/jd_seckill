5d6b1ed42578c99ff9573052acc72a7ad0ec03ca
郑秋爽 <Jarvanzheng@163.com>
2022-08-25 14:01:07 +0800

    暂存

--- src/block/productEnquiries.js
+++ src/block/productEnquiries.js
@@ -79,7 +79,7 @@ class Goods {
 		return result[goodId]
 	}
 
-	//
+	// 获取购物车信息
 	async getUserCartInfo() {
 		const result = await got
 			.post('https://api.m.jd.com/api', {
@@ -97,8 +97,135 @@ class Goods {
 				}
 			})
 			.json()
+
 		console.log(result.resultData)
-		// return this.getGoodInfo(result.data.resultData, user)
+		return this.getGoodInfo(result.resultData)
+	}
+	// 解析购物车商品信息
+	getGoodInfo(resultData) {
+		const user = this.userData
+		let shopList = []
+		let goodInfo = {}
+
+		if (!resultData) {
+			console.log('购物车无商品')
+			// resultData = {}
+			// if (!user.outLogin) {
+			// 	let ids = this.getTempMsgUserId(user.unick)
+			// 	ids.forEach((e) => {
+			// 		this.temp_msg(e, '登陆状态已过期', `京东账号：${user.unick}`, '点击跳转扫码登陆', '', '', true)
+			// 	})
+			// }
+			// user.outLogin = true
+		}
+		if (resultData.cartInfo) {
+			let vendors = resultData.cartInfo.vendors
+			vendors.forEach((e) => {
+				let shopitem = e.sorted[0].item
+				let sType = 1
+				let num = shopitem.Num
+				let Id = e.shopId
+				let items = []
+				e.sorted.forEach((o) => {
+					let obj = {
+						Id: o.item.Id,
+						skuUuid: o.item.skuUuid,
+						useUuid: o.item.useUuid,
+						num: o.item.Num,
+						Name: o.item.Name
+					}
+					items.push(obj)
+					goodInfo[o.Id] = obj
+				})
+				shopList.push({
+					sType,
+					num,
+					Id,
+					items: items
+				})
+			})
+		}
+		console.log({ shopList, goodInfo })
+		return { shopList, goodInfo }
+	}
+
+	// 清空购物车
+	async removeUserCart(user) {
+		let { shopList } = await this.getUserCartInfo(user)
+		if (shopList.length == 0) {
+			console.log(`${user.unick} 购物车已空${shopList.length}`)
+			return true
+		}
+		console.log('shopList',shopList)
+
+		let url = 'https://api.m.jd.com/api'
+		let body = {
+			operations: [
+				{
+					carttype: '4',
+					TheSkus: [],
+					ThePacks: [
+						{
+							Id: 106417096782,
+							num: 1,
+							sType: 13,
+							TheSkus: [{ Id: '10025678773125', num: 1, skuUuid: '1194226238944649706634989568', useUuid: false }]
+						}
+					]
+				}
+			],
+			serInfo: { area: '19_1601_36953_62867' }
+		}
+		body.serInfo.area = g_data.areaId
+		body.operations[0].ThePacks = []
+		console.log(body)
+		shopList.forEach((e) => {
+			let obj = {
+				num: e.num,
+				sType: e.sType,
+				Id: e.Id,
+				TheSkus: []
+			}
+			e.items.forEach((item) => {
+				obj.TheSkus.push({
+					num: item.num,
+					Id: item.Id,
+					skuUuid: item.skuUuid,
+					useUuid: item.useUuid
+				})
+			})
+			body.operations[0].ThePacks.push(obj)
+		})
+		// let delParam = [{id:10025522073879,s:false,vs:false},{id:10025678773125,s:false,vs:false},{id:70196142298,s:false,vs:false}]
+		let delParam = []
+		shopList.forEach((e) => {
+			e.items.forEach((item) => {
+				delParam.push({
+					id: item.Id,
+					s: false,
+					vs: false
+				})
+			})
+		})
+		console.log(delParam)
+		const result = await request({
+			method: 'post',
+			url: url,
+			headers: Object.assign(user.header, {
+				origin: 'https://cart.jd.com',
+				referer: 'https://cart.jd.com/',
+				cookie: user.cookieData.join('')
+			}),
+			params: {
+				functionId: 'pcCart_jc_cartRemove',
+				appid: 'JDC_mall_cart',
+				loginType: '3',
+				body: body
+			}
+		})
+		// console.log(result.data)
+		// console.log(result.data.resultData)
+		// console.log(JSON.stringify(result.data.resultData))
 	}
 }
 
